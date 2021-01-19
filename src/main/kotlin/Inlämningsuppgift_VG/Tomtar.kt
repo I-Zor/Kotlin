81115ba108aff6e1786b69327ea6a8f0a68267f2
IvonaZoricic <ivona.zoricic@gmail.com>
2021-01-19 17:59:18 +0100

    Initial commit

---
+++ src/main/kotlin/Inlämningsuppgift_VG/Tomtar.kt
@@ -0,0 +1,36 @@
+package Inlämningsuppgift_VG
+
+class Tomtar {
+
+    //I varje par är tomten key och hans chef value.
+    // En chef kan ha flera underordnade men varje tomte har bara en överordnade.
+
+    val hierarchy = mapOf("Glader" to "Tomten",
+            "Butter" to "Tomten",
+            "Tröger" to "Glader",
+            "Trötter" to "Glader",
+            "Blyger" to "Glader",
+            "Rådjuret" to "Butter",
+            "Nyckelpigan" to "Butter",
+            "Haren" to "Butter",
+            "Räven" to "Butter",
+            "Skumtomten" to "Trötter",
+            "Gråsuggan" to "Räven",
+            "Myran" to "Räven",
+            "Dammråttan" to "Skumtomten",
+            "Bladlusen" to "Myran")
+
+}
+
+fun Tomtar.getSuperiors(namn: String): List<String> {
+    if (hierarchy.containsKey(namn))
+        return getSuperiors(hierarchy.getValue(namn)).plus(hierarchy.getValue(namn))
+    else
+        return listOf()
+}
+
+fun main() {
+    val t = Tomtar()
+    println(t.getSuperiors("Dammråttan"))
+}
+
